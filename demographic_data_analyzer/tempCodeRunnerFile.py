b62a22558585287748092e09f6d4ed803446d8bd
iVishav48 <vishav4848@gmail.com>
2025-12-26 16:07:41 +0530

    add demographic analyzer project

---
+++ demographic_data_analyzer/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people without advanced education make more than 50K?
+
+    # with and without `Bachelors`, `Masters`, or `Doctorate`
